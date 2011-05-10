19d5f9b088886f342713e6cc5ff347d665ed610f
yoavniran@gmail.com <yoavniran@gmail.com@73b5e0d0-6e95-2095-0101-50d48f5dc44c>
2011-05-10 11:29:36 +0000

    -	I changed the assemblies names so instead of PowerTools2011 – Editor its PowerTool2011.Editor -	Create a PowerTools2011.Common assembly to contain all base functionality. -	I created base classes for a power tools extension pages for all popups to inherit from, currently empty. -	I took Chris’ example for the progress bar and modified it so it follows the more GUI Extension way for creating a popup, both in the code and in the editor configuration. Really cool by the way Chris! I also renamed the command, to have the namespace PowerTools2011.Commands. We should have one namespace for our tools. -	I restructured the folders in the editor project to have a more intuitive and self-explanatory structure. -	I’ve added a base resource group to the editor config including some dependencies such as jQuery and jQueryUI, along with my utils.js which has a few useful, reusable functions.

---
+++ PowerTools2011 - Editor/PowerTools/Client/Example/ExampleCommand.js
@@ -0,0 +1,27 @@
+﻿Type.registerNamespace("PowerTools2011.Commands");
+
+PowerTools2011.Commands.Example = function () 
+{
+	Type.enableInterface(this, "PowerTools2011.Commands.Example");
+    this.addInterface("Tridion.Cme.Command", ["Example"]);
+};
+
+PowerTools2011.Commands.Example.prototype.isAvailable = function (selection)
+{
+    return true;
+};
+
+PowerTools2011.Commands.Example.prototype.isEnabled = function (selection)
+{
+    return true;
+};
+
+PowerTools2011.Commands.Example.prototype._execute = function (selection)
+{
+	var uriSelection = selection.getItem(0);
+	var PopUpUrl = $ptUtils.expandPath("/powertools/client/example/example.aspx") + "?id=" + uriSelection;  //"/WebUI/Editors/PowerTools2011/PowerTools/Example/Example.aspx" + "?id=" + uriSelection;
+	var popup = $popup.create(PopUpUrl, "toolbar=no,width=600,height=300,resizable=false,scrollbars=false", null);
+	popup.open();
+
+};
+
---
+++ PowerTools2011 - Editor/PowerTools/Client/Shared/Scripts/Types.js
@@ -0,0 +1,24 @@
+﻿
+///
+/// do a Deep clone of the object into a new instance
+/// (tried adding 'deepClone' to Object.prototye but GUI threw a bunch of errors)
+Object.deepCopy = function (existing)
+{
+	var newObj = (existing instanceof Array) ? [] : {};
+
+	for (i in existing)
+	{
+		if (i == "deepClone") continue;
+
+		if (existing[i] && typeof existing[i] == "object")
+		{
+			newObj[i] = Object.deepCopy(existing[i]); //.deepClone();
+		}
+		else
+		{
+			newObj[i] = existing[i];
+		}
+	}
+
+	return newObj;
+};
