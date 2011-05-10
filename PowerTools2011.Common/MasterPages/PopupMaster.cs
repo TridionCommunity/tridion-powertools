94357a961d0e9a37be314f3b4ecde41988573fa0
yoavniran@gmail.com <yoavniran@gmail.com@73b5e0d0-6e95-2095-0101-50d48f5dc44c>
2011-05-10 11:27:19 +0000

    -	I changed the assemblies names so instead of PowerTools2011 – Editor its PowerTool2011.Editor -	Create a PowerTools2011.Common assembly to contain all base functionality. -	I created base classes for a power tools extension pages for all popups to inherit from, currently empty. -	I took Chris’ example for the progress bar and modified it so it follows the more GUI Extension way for creating a popup, both in the code and in the editor configuration. Really cool by the way Chris! I also renamed the command, to have the namespace PowerTools2011.Commands. We should have one namespace for our tools. -	I restructured the folders in the editor project to have a more intuitive and self-explanatory structure. -	I’ve added a base resource group to the editor config including some dependencies such as jQuery and jQueryUI, along with my utils.js which has a few useful, reusable functions.

---
+++ PowerTools2011.Common/MasterPages/PopupMaster.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI;
+
+namespace PowerTools2011.Common.MasterPages
+{
+	public class PopupMaster : MasterPage
+	{
+	}
+}
---
+++ PowerTools2011.Common/Pages/PowerToolsPageBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Tridion.Web.UI.Controls;
+
+namespace PowerTools2011.Common.Pages
+{
+	public abstract class PowerToolsPageBase : TridionPage
+	{
+	}
+}
