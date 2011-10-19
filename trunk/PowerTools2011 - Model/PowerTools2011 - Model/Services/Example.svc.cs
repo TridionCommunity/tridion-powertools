3b61c9326561ef99f82699f3b9ed62970a149467
pkjaer.sdl@gmail.com <pkjaer.sdl@gmail.com@73b5e0d0-6e95-2095-0101-50d48f5dc44c>
2011-10-19 18:29:54 +0000

    I moved everything related to web services into the Model project and removed the Services project. It is now a UI Model, so you need to configure it in System.config and add a virtual directory for it.

---
+++ trunk/PowerTools2011 - Model/PowerTools2011 - Model/Services/Example.svc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ServiceModel.Web;
+using System.Threading;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using PowerTools2011.Model.Services.Progress;
+
+
+namespace PowerTools2011.Model.Services
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single, ConcurrencyMode = ConcurrencyMode.Multiple)]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Required)]
+    [ServiceContract(Namespace = "PowerTools2011.Model.Services")]
+    public class Example : BaseService
+    {
+        [OperationContract, WebGet(ResponseFormat = WebMessageFormat.Json)]
+        public ServiceProcess Execute()
+        {
+            return ExecuteAsync(null);
+        }
+
+        [OperationContract, WebGet(ResponseFormat = WebMessageFormat.Json)]
+        public override ServiceProcess GetProcessStatus(string Id)
+        {
+            return base.GetProcessStatus(Id);
+        }
+
+        public override void Process(ServiceProcess process, object arguments)
+        {
+            while (process.PercentComplete < 100)
+            {
+                process.IncrementCompletePercentage();
+                process.SetStatus("Loading...");
+
+                Random rnd = new Random(DateTime.Now.GetHashCode());
+                Thread.Sleep(((int)(rnd.NextDouble() * 4) + 10) * 10);
+            }
+            process.Complete();
+        }
+    }
+}
---
+++ trunk/PowerTools2011 - Model/PowerTools2011 - Model/Services/ServiceResponseBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace PowerTools2011.Model.Services
+{
+	[DataContract]
+	public class ServiceResponseBase
+	{
+		[DataMember]
+		public string Message { get; set; }
+
+		[DataMember]
+		public bool Success { get; set; }
+
+		[DataMember]
+		public string ProcessId { get; set; }
+	}
+}
