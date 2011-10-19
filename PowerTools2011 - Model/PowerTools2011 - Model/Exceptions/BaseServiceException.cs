6d07a368c7dec7565a442b2c5bea1ef91ef709d0
pkjaer.sdl@gmail.com <pkjaer.sdl@gmail.com@73b5e0d0-6e95-2095-0101-50d48f5dc44c>
2011-10-19 19:24:53 +0000

    Another directory that was copied and so didn't make it into the previous commit.

---
+++ PowerTools2011 - Model/PowerTools2011 - Model/Exceptions/BaseServiceException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PowerTools2011.Model.Services.Exceptions
+{
+    public class BaseServiceException : Exception
+    {
+        public BaseServiceException() : base()
+        {
+        }
+
+        public BaseServiceException(string message) : base(message)
+        {
+        }
+    }
+}
