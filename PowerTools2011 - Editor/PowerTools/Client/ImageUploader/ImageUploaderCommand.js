f3176094b68be13c2f9b0088d94db27ea44b94b3
s990330 <s990330@73b5e0d0-6e95-2095-0101-50d48f5dc44c>
2011-10-19 20:37:32 +0000

    changed the isEnabled and isAvailable behaviour. Now based on the selection in the TREEVIEW.

--- PowerTools2011 - Editor/PowerTools/Client/ImageUploader/ImageUploaderCommand.js
+++ PowerTools2011 - Editor/PowerTools/Client/ImageUploader/ImageUploaderCommand.js
@@ -7,29 +7,20 @@ PowerTools2011.Commands.ImageUploader = function ()
     this.addInterface("PowerTools2011.ToolBase", ["ImageUploader"]);
 };
 
-PowerTools2011.Commands.ImageUploader.prototype.isAvailable = function (selection)
-{
-    //Only show the button if a single FOLDER is selected
-    if (selection.getCount() == 1) {
-        var itemType = $models.getItemType(selection.getItem(0));
-        var item = $models.getItem(selection.getItem(0))
-        if (itemType == $const.ItemType.FOLDER) {
-            return true;
-        }
-    }
-    return false;
+PowerTools2011.Commands.ImageUploader.prototype.isAvailable = function (selection) {
+    //    //Only show the button if a single FOLDER is selected
+    //    if (selection.getCount() == 1) {
+    //        var itemType = $models.getItemType(selection.getItem(0));
+    //        var item = $models.getItem(selection.getItem(0))
+    //        if (itemType == $const.ItemType.FOLDER) {
+    //            return true;
+    //        }
+    //    }
+    return this._defineEnabled();
 };
 
 PowerTools2011.Commands.ImageUploader.prototype.isEnabled = function (selection) {
-   
-    if (selection.getCount() == 1) {
-        var itemType = $models.getItemType(selection.getItem(0));
-        //var item = $models.getItem(selection.getItem(0))
-        if (itemType == $const.ItemType.FOLDER) {
-            return true;
-        }
-    }
-    return false;
+    return this._defineEnabled();
 };
 
 PowerTools2011.Commands.ImageUploader.prototype._execute = function (selection)
@@ -44,3 +35,12 @@ PowerTools2011.Commands.ImageUploader.prototype._execute = function (selection)
     popup.open();
 };
 
+PowerTools2011.Commands.ImageUploader.prototype._defineEnabled = function () {
+    var treeView = $controls.getControl($("#DashboardTree"), "Tridion.Controls.FilteredTree");
+    var selection = treeView.getSelection().getItem(0);
+    var itemType = $models.getItemType(selection);
+    if (itemType == $const.ItemType.FOLDER) {
+        return true;
+    }
+    return false;
+}
