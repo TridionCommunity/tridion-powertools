432f465529ef70a6e96493f81904c55c0a69b09f
s990330 <s990330@73b5e0d0-6e95-2095-0101-50d48f5dc44c>
2011-10-24 19:37:12 +0000

    Added utils for handling TcmUri's.

---
+++ PowerTools.Model/Utils/StringExtensions.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text.RegularExpressions;
+
+namespace PowerTools.Model.Utils
+{
+	public static class StringExtensions
+	{
+		public static int PublicationId(this String str)
+		{
+
+			Regex re = new Regex(@"tcm:(\d+)-(\d+)-?(\d*)-?v?(\d*)");
+			Match m = re.Match(str);
+			if (m.Success)
+			{
+				return Convert.ToInt32(m.Groups[1].Value);
+			}
+
+			return 0;
+
+		}
+
+		public static int ItemId(this String str)
+		{
+
+			Regex re = new Regex(@"tcm:(\d+)-(\d+)-?(\d*)-?v?(\d*)");
+			Match m = re.Match(str);
+			if (m.Success)
+			{
+				return Convert.ToInt32(m.Groups[2].Value);
+			}
+
+			return 0;
+
+		}
+
+		public static int ItemTypeId(this String str)
+		{
+			Regex re = new Regex(@"tcm:(\d+)-(\d+)-?(\d*)-?v?(\d*)");
+			Match m = re.Match(str);
+			if (m.Groups.Count > 3 && !string.IsNullOrEmpty(m.Groups[3].Value))
+			{
+				return Convert.ToInt32(m.Groups[3].Value);
+			} else
+			{
+				return 16;
+			}
+		}
+
+		public static int Version(this String str)
+		{
+			Regex re = new Regex(@"tcm:(\d+)-(\d+)-?(\d*)-?v?(\d*)");
+			Match m = re.Match(str);
+			if (m.Groups.Count > 4 && !string.IsNullOrEmpty(m.Groups[4].Value))
+			{
+				return Convert.ToInt32(m.Groups[4].Value);
+			} else
+			{
+				return 0;
+			}
+		}
+	}
+}
