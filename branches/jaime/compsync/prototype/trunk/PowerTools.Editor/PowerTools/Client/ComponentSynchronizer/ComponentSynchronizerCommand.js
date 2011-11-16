9d1835446e4c8d9ef64052db50e5db135fc256ce
jsantosalcon <jsantosalcon@73b5e0d0-6e95-2095-0101-50d48f5dc44c>
2011-11-16 22:07:32 +0000

    Added global variables, passing to the popup: schema, publication, and temporary folder within the window dialog arguments

--- branches/jaime/compsync/prototype/trunk/PowerTools.Editor/PowerTools/Client/ComponentSynchronizer/ComponentSynchronizerCommand.js
+++ branches/jaime/compsync/prototype/trunk/PowerTools.Editor/PowerTools/Client/ComponentSynchronizer/ComponentSynchronizerCommand.js
@@ -9,6 +9,8 @@ PowerTools.Commands.ComponentSynchronizer = function () {
     this.addInterface("Tridion.Cme.Command", ["ComponentSynchronizer"]);
     this.addInterface("PowerTools.ToolBase", ["ComponentSynchronizer"]);
     this._isPopupOpen = false;
+    this._currentSchema = null;
+    this._currentPublication = null;
 };
 
 
@@ -39,7 +41,6 @@ PowerTools.Commands.ComponentSynchronizer.prototype._execute = function (selecti
     if (this._isPopupOpen) {
         return;
     }
-
     var validSelection = this.isValidSelection(selection);
 
     if (validSelection) {
@@ -47,19 +48,25 @@ PowerTools.Commands.ComponentSynchronizer.prototype._execute = function (selecti
         var baseElement = $("#contentsplitter_container");
         var iFrame = $("#CustomPagesFrame");
         var self = this;
-        
+
         var PopUpUrl = $ptUtils.expandPath("/PowerTools/Client/ComponentSynchronizer/ComponentSynchronizer.aspx"); // +"#folderId=" + uriSelection;
-        this._popup = $popup.create(PopUpUrl, "toolbar=no,width=750px,height=630px,resizable=false,scrollbars=false", { sel: selection });
-
-        $evt.addEventHandler(this._popup, "unload", this.getDelegate(this._onPopupCanceled));       
-
+        //TODO: USING ROOT FOLDER FOR TEMPORARY LOCATION
         
+
+        var publication = $models.getItem(this._currentPublication);
+        this._rootFolder = publication.getRootFolderId();
+
+        $log.message("Opening Popup for Schema: " + this._currentSchema);
+        this._popup = $popup.create(PopUpUrl, "toolbar=no,width=750px,height=630px,resizable=false,scrollbars=false", { sel: selection, schema: this._currentSchema, pub: this._currentPublication, folder: this._rootFolder});
+        $evt.addEventHandler(this._popup, "unload", this.getDelegate(this._onPopupCanceled));
+
+
         this._popup.open();
         this._isPopupOpen = true;
 
     }
     else {
-        var msg = $messages.createMessage("Tridion.Cme.Model.WarningMessage", "SELECTION MUST BE EITHER: ", "- A Schema \r\n - A Component \r\n - A list of Components based on the same Schema \r\n ", true, true);
+        var msg = $messages.createMessage("Tridion.Cme.Model.WarningMessage", "SELECTION MUST BE EITHER: ", "- A Schema \r\n - A Component \r\n - Multiple Components based on the same Schema \r\n ", true, true);
         $messages.registerMessage(msg);
     }
 
@@ -86,8 +93,9 @@ PowerTools.Commands.ComponentSynchronizer.prototype.isValidSelection = function 
     var items = sel.getItems();
     if (items.length > 1) {
         var firstSchema = '';
+        var currentPub = '';
         for (var i = 0, len = items.length; i < len; i++) {
-            
+
             var itemId = sel.getItem(i);
             var item = $models.getItem(itemId);
 
@@ -97,6 +105,7 @@ PowerTools.Commands.ComponentSynchronizer.prototype.isValidSelection = function 
                 }
                 if (i == 0) {
                     firstSchema = item.getSchema().getId();
+                    currentPub = item.getPublicationId();
                 }
                 if (i > 0 && item.getSchema().getId() != firstSchema) {
                     return false;
@@ -104,6 +113,8 @@ PowerTools.Commands.ComponentSynchronizer.prototype.isValidSelection = function 
             }
 
         }
+        this._currentSchema = firstSchema;
+        this._currentPubilcation = currentPub;
     }
     return true;
 }
@@ -122,11 +133,18 @@ PowerTools.Commands.ComponentSynchronizer.prototype._defineEnabled = function (s
         //Nothing Selected
         return false;
     } else if (items.length == 1) {
-        var itemId = selection.getItem(0);        
+        var itemId = selection.getItem(0);
         var item = $models.getItem(itemId);
         if (item) {
             if (item.getItemType() != $const.ItemType.SCHEMA && item.getItemType() != $const.ItemType.COMPONENT) {
                 return false;
+            } else {
+                this._currentPublication = item.getPublicationId();
+                if (item.getItemType() == $const.ItemType.SCHEMA) {
+                    this._currentSchema = item.getId();                    
+                } else {
+                    this._currentSchema = item.getSchema().getId();
+                }
             }
         }
     } else {
