4a421dbc099e97e0cb08e37f180348a9293ca5e3
johnwinter <johnwinter@73b5e0d0-6e95-2095-0101-50d48f5dc44c>
2012-01-07 15:45:41 +0000

    Fixed issues related to the selected item when the powertool is launched - previously it was using the location id from the querystring in the cme

--- trunk/PowerTools.Editor/PowerTools/Client/PagePublisher/PagePublisherCommand.js
+++ trunk/PowerTools.Editor/PowerTools/Client/PagePublisher/PagePublisherCommand.js
@@ -17,10 +17,11 @@ PowerTools.Commands.PagePublisher.prototype.isEnabled = function (selection)
     return this._defineEnabled(selection);
 };
 
+
 PowerTools.Commands.PagePublisher.prototype._execute = function (selection) {
 
     // get the id of the selected item
-    var uriSelection = $url.getHashParam("locationId");  //selection.getItem(0);
+    var uriSelection = this._selectedItem(selection); //$url.getHashParam("locationId");  //selection.getItem(0);
 
     // build the pop up url for the publish dialog and open it
     var PopUpUrl = $ptUtils.expandPath("/PowerTools/Client/PagePublisher/PagePublisher.aspx") + "#locationId=" + uriSelection;
@@ -28,32 +29,35 @@ PowerTools.Commands.PagePublisher.prototype._execute = function (selection) {
     popup.open();
 };
 
-PowerTools.Commands.PagePublisher.prototype._defineEnabled = function (selection) {
-
-/*
-    var selected;
-
+PowerTools.Commands.PagePublisher.prototype._selectedItem = function (selection) {
     switch (selection.getCount()) {
         case 0: // check the Tree selection
             var treeView = $controls.getControl($("#DashboardTree"), "Tridion.Controls.FilteredTree");
-            selected = treeView.getSelection().getItem(0);
+            return treeView.getSelection().getItem(0);
             break;
 
         case 1: // multiple items selected in the main list
-            selected = selection.getItem(0);
+            return selection.getItem(0);
             break;
 
         default:
             return null;
             break;
     }
+}
 
-*/
+PowerTools.Commands.PagePublisher.prototype._defineEnabled = function (selection) {
 
-    var itemType = $models.getItemType($url.getHashParam("locationId"));
-
-    if (itemType == $const.ItemType.STRUCTURE_GROUP || itemType == $const.ItemType.PUBLICATION) {
-        return true;
+    var item = this._selectedItem(selection);
+    if (item != null)
+    {
+        switch ($models.getItemType(item))
+        {
+            case $const.ItemType.PUBLICATION:
+            case $const.ItemType.STRUCTURE_GROUP:
+                return true;
+                break;
+        }
     }
     return false;
 }
