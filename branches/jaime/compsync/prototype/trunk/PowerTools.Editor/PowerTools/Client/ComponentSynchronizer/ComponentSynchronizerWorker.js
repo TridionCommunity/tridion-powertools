97494c999a223f1dad5ea9d15e0499b32e61971e
jsantosalcon <jsantosalcon@73b5e0d0-6e95-2095-0101-50d48f5dc44c>
2012-01-30 14:52:11 +0000

    Component Synchronizer (Alternateive 2)

--- branches/jaime/compsync/prototype/trunk/PowerTools.Editor/PowerTools/Client/ComponentSynchronizer/ComponentSynchronizerWorker.js
+++ branches/jaime/compsync/prototype/trunk/PowerTools.Editor/PowerTools/Client/ComponentSynchronizer/ComponentSynchronizerWorker.js
@@ -1,24 +1,28 @@
 ﻿Type.registerNamespace("PowerTools");
 
 // Constructs a new worker. Reads item TcmUri from URL.
-PowerTools.ComponentSynchronizerWorker = function ()
-{
+PowerTools.ComponentSynchronizerWorker = function () {
     this.properties = [];
     var p = this.properties;
     p.processId = null;
     p.itemId = $url.getHashParam("id");
     p.pollInterval = 500; //Milliseconds between each call to check the status of a process
+    p.tableRefreshInterval = 500;
 };
 
 // Initiate the call to the service to retrieve all Application Data for the given item
-PowerTools.ComponentSynchronizerWorker.prototype.execute = function (sel, ref)
-{
+PowerTools.ComponentSynchronizerWorker.prototype.execute = function (sel, ref) {
     $log.message("Executing ComponentSynchronizer...");
 
     // prepare UI
-    //JAIME: NOT NEEDED: $j('#tbody').html("<tr class=\"row0\"><td colspan=\"3\">Loading...</td></tr>");
-    $j('#ProgressBar').css({ 'width': '1%', 'display': 'block' });
-    $j('#ProgressStatus').html("Progress");
+    //JAIME: NOT NEEDED: REMOVE MODAL DIALOG (MAKE IT OPTIONAL)
+    $j('#ModalDialog').css({ 'width': '1%', 'display': 'none' });    
+    $j('#pbImage').css({ 'background-position': '-120px 50%' });
+    $j('#processComplete').html("&nbsp;- 0%");
+    
+
+    
+
     //JAIME: CLOSE BUTTON INSTEAD: $j('#CloseDialog').hide();
 
     var dialog = $j("#dialog");
@@ -39,7 +43,8 @@ PowerTools.ComponentSynchronizerWorker.prototype.execute = function (sel, ref)
     var onFailure = null;
     var context = null;
 
-    PowerTools.Model.Services.ComponentSynchronizer.Execute(sel, ref);
+    $log.message("Calling WS for: " + sel.getItems() + " Ref: " + ref);
+    PowerTools.Model.Services.ComponentSynchronizer.Execute(sel.getItems(), ref, onSuccess, onFailure);
 };
 
 // Once the call for app data has been initialised, this call back will start polling for progress status updates
@@ -51,76 +56,80 @@ PowerTools.ComponentSynchronizerWorker.prototype._onExecuteStarted = function (r
     }
 };
 
-// Initiate the call for actual data. This call back is called once the service Execute method completed 
-// and data is ready on the service to be picked up.
-PowerTools.ComponentSynchronizerWorker.prototype._getData = function (id) {
-    if (id != "") {
-        alert('Done!');
-    }
-};
-
-// Call back handling the actual response with Application Data data. Update the UI.
-PowerTools.ComponentSynchronizerWorker.prototype._handleDataResponse = function (response)
-{
-    var p = this.properties;
-    var content = "";
-    var i = 0;
-    response.each(function (elem)
-    {
-        content += "<tr class=\"row" + (i % 2) + "\"><td>" + $ptUtils.wbr($ptUtils.htmlEncode(elem.ApplicationId), 12) +
-                "</td><td>" + $ptUtils.wbr($ptUtils.htmlEncode(elem.Value), 12) +
-                "</td><td>" + $ptUtils.wbr($ptUtils.htmlEncode(elem.Type), 12) +
-                "</td</tr>";
-        i++;
-    });
-
-    if (!content)
-    {
-        content = "<tr class=\"row0\"><td colspan=\"3\" align=\"center\">No data found</td></tr>";
-    }
-
-    $j('#tbody').html(content);
-
-    // hide progress bar
-    $j('#mask, .window').hide();
-    $j('#ProgressStatus').html("");
-    $j('#ProgressBar').css({ 'width': 0 + '%', 'display': 'none' });
-};
-
 // Initiate progress status update requests to the service. Schedules itself to run again after pollInterval is reached.
-PowerTools.ComponentSynchronizerWorker.prototype._pollStatus = function (id)
-{
+PowerTools.ComponentSynchronizerWorker.prototype._pollStatus = function (id) {
     var onSuccess = Function.getDelegate(this, this._handleStatusResponse);
     var onFailure = null;
     var context = null;
 
-    var callback = function ()
-    {
-        $log.debug("Checking the status of process #" + id);
-        PowerTools.Model.Services.ComponentSynchronizer.GetProcessStatus(id, onSuccess, onFailure, context, false);
-    };
+    var onSuccess2 = Function.getDelegate(this, this._handleStatusResponse);
+    var onFailure2 = null;
+    var context2 = null;
 
-    setTimeout(callback, this.properties.pollInterval);
+
+    var p = this.properties;
+
+
+    var callback = function () {
+        //$log.debug("Checking the status of process #" + id);
+        PowerTools.Model.Services.ComponentSynchronizer.GetProcessStatus(id, onSuccess, onFailure, context, false);
+    };   
+
+    
+    p.statusPollFunction = setTimeout(callback, this.properties.pollInterval);
 }
 
+
 // Call back that handles progress status reponses. If process completed, call _getData to retrieve the data. Update UI.
-PowerTools.ComponentSynchronizerWorker.prototype._handleStatusResponse = function (result)
-{
-    $log.message("Handle Status Response...");
+PowerTools.ComponentSynchronizerWorker.prototype._handleProcessedResponse = function (result) {
+
+    var callback2 = function () {
+        var rows = $j('#UsedInList_frame_details').contents().find('tr[id^="item_"]');
+        for (var i = 0; i < 10; i++) {
+            var origId = $j(rows[i]).attr('id');
+            var tcmURI = origId.replace("item_", "");
+            var onSuccess2 = Function.getDelegate(this, this._handleProcessedResponse);
+            var onFailure2 = null;
+            $log.message("id: " + id + " uri:" + tcmURI);
+            PowerTools.Model.Services.ComponentSynchronizer.IsItemProcessed(id, tcmURI, onSuccess2, onFailure2, context2, false);
+        }
+    }
+    
+    alert("URI: " + result.ComponentURI);
+    var row = $j('#UsedInList_frame_details').contents().find('tr[id^="' + result.ComponentURI + '"]');
+    $j(rows).attr('id', 'processed_' + origId);
+    var column = $j("td", $j(row))[2];
+    $j(column).html("Jaime " + itemId);
+}
+
+
+
+
+// Call back that handles progress status reponses. If process completed, call _getData to retrieve the data. Update UI.
+PowerTools.ComponentSynchronizerWorker.prototype._handleStatusResponse = function (result) {
+    //$log.message("Handle Status Response...");
     var p = this.properties;
     p.processId = result.Id;
-    $j('#ProgressStatus').html(result.Status);
-    $j('#ProgressBar').css({ 'width': result.PercentComplete + '%', 'display': 'block' });
+    var percentage = -120 + result.PercentComplete;
+    var value = percentage + 'px 50%';
+    $j('#pbImage').css({ 'background-position': value });
+    $j('#processComplete').html("&nbsp;- " + result.PercentComplete + "% - " + result.Status);
 
-    if (result.PercentComplete < 100)
-    {
+
+    if (result.PercentComplete < 100) {
         this._pollStatus(p.processId);
     }
-    else
-    {
-        this._getData(p.processId);
-        $j('#ProgressStatus').html(result.Status);
+    else {
         p.processId = "";
+        clearTimeout(p.statusPollFunction);
+        clearTimeout(p.processedItemsPollFunction);        
+
+        $j('#UsedInList_frame_details').contents().find('tr[id^="item_"]').each(function (index) {
+            var origId = $j(this).attr('id');
+            $j(this).attr('id', 'processed_' + origId);
+            var column = $j("td", $j(this))[2];
+            $j(column).html("Jaime");
+        });
     }
 }
 
