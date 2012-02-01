f376ac79b4d76aac91066d27326041231c5bc32b
stanislav.kroschenko@gmail.com <stanislav.kroschenko@gmail.com@73b5e0d0-6e95-2095-0101-50d48f5dc44c>
2012-02-01 13:13:38 +0000

    Added new PowerTool DocumentItem - this tool aimed to create Schema Definition for any Tridion schema.

---
+++ PowerTools.Editor/PowerTools/Client/DocumentItem/DocumentItemCommand.js
@@ -0,0 +1,32 @@
+Type.registerNamespace("PowerTools.Commands");
+
+PowerTools.Commands.DocumentItem = function () {
+    Type.enableInterface(this, "PowerTools.Commands.DocumentItem");
+    this.addInterface("Tridion.Cme.Command", ["DocumentItem"]);
+    /* Do we really need this?*/
+    this.addInterface("PowerTools.ToolBase", ["DocumentItem"]);
+};
+
+PowerTools.Commands.DocumentItem.prototype.isAvailable = function (selection)
+{
+    if (selection.getItems().length > 1)
+        return false;
+    var itemID = selection.getItem(0);
+	if ($models.getItemType(itemID) != $const.ItemType.SCHEMA) {
+		return false;
+	}
+    return true;
+};
+
+PowerTools.Commands.DocumentItem.prototype.isEnabled = function (selection)
+{    
+    return this.isAvailable(selection);
+};
+
+PowerTools.Commands.DocumentItem.prototype._execute = function ExportToWord$_execute(selection) {
+
+    var itemID = selection.getItem(0);
+    //var itemType = $models.getItemType(itemID);
+
+    window.location = document.location.protocol + "//" + document.location.host + "/WebUI/SchemaDefinition.docx?id=" + itemID;
+};
