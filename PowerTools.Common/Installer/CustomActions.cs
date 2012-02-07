8fea0f3d3900f7b4f33daa3a401307ba50031443
pkjaer.sdl@gmail.com <pkjaer.sdl@gmail.com@73b5e0d0-6e95-2095-0101-50d48f5dc44c>
2012-02-07 15:36:34 +0000

    Updated the installer to create the necessary virtual directories in IIS.

--- PowerTools.Common/Installer/CustomActions.cs
+++ PowerTools.Common/Installer/CustomActions.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.ComponentModel;
+using System.DirectoryServices;
+using System.Globalization;
+using System.Linq;
 using System.Security.Permissions;
 
 
@@ -8,6 +12,15 @@ namespace PowerTools.Common.Installer
     [RunInstaller(true)]
     public partial class CustomActions : System.Configuration.Install.Installer
     {
+        const string SchemaNameWebsite = "IIsWebServer";
+        const string SchemaNameVirtualDir = "IIsWebVirtualDir";
+        const string SchemaNameWebDirectory = "IIsWebDirectory";
+        const string KeyEditorVirtualDirectoryPath = "PowerTools_EditorVirtualDirectoryPath";
+        const string KeyModelVirtualDirectoryPath = "PowerTools_ModelVirtualDirectoryPath";
+
+        protected IDictionary state;
+
+
         public CustomActions()
         {
             InitializeComponent();
@@ -16,25 +29,160 @@ namespace PowerTools.Common.Installer
         [SecurityPermission(SecurityAction.Demand)]
         public override void Install(IDictionary stateSaver)
         {
+            state = stateSaver;
             base.Install(stateSaver);
+            CreateVirtualDirectories();
         }
 
         [SecurityPermission(SecurityAction.Demand)]
         public override void Commit(IDictionary savedState)
         {
+            state = savedState;
             base.Commit(savedState);
         }
 
         [SecurityPermission(SecurityAction.Demand)]
         public override void Rollback(IDictionary savedState)
         {
+            state = savedState;
             base.Rollback(savedState);
+            DeleteVirtualDirectories();
         }
 
         [SecurityPermission(SecurityAction.Demand)]
         public override void Uninstall(IDictionary savedState)
         {
+            state = savedState;
             base.Uninstall(savedState);
+            DeleteVirtualDirectories();
+        }
+
+
+        /// <summary>
+        /// Gets a virtual directory (by name) underneath the given directory.
+        /// </summary>
+        /// <param name="parent">The parent entry to search.</param>
+        /// <param name="childName">The name of the virtual directory to return.</param>
+        /// <returns></returns>
+        protected DirectoryEntry GetChildVirtualDirectory(DirectoryEntry parent, string childName)
+        {
+            if (parent == null)
+            {
+                throw new ArgumentNullException("parent");
+            }
+
+            return (from entry in parent.Children.Cast<DirectoryEntry>()
+                    let entryType = entry.SchemaClassName
+                    where entry.Name == childName
+                    where entryType == SchemaNameVirtualDir || entryType == SchemaNameWebDirectory
+                    select entry).FirstOrDefault();
+        }
+
+
+        /// <summary>
+        /// Creates the PowerTools virtual directories under the existing WebUI\Editors and WebUI\Models virtual directories.
+        /// </summary>
+        protected void CreateVirtualDirectories()
+        {
+            const string TargetDirectoryName = "PowerTools";
+
+            string websiteId = Context.Parameters["CME_WEBSITE_ID"] as string;
+            if (string.IsNullOrEmpty(websiteId))
+            {
+                throw new Exception(Resources.ErrorWebsiteIdNotSet);
+            }
+
+            string targetDir = Context.Parameters["TARGETDIR"] as string;
+            if (string.IsNullOrEmpty(targetDir))
+            {
+                throw new Exception(Resources.ErrorTargetDirNotSet);
+            }
+
+            using (DirectoryEntry w3svc1 = new DirectoryEntry("IIS://Localhost/W3SVC"))
+            {
+                var site = w3svc1.Children.Find(websiteId, SchemaNameWebsite);
+                if (site == null)
+                {
+                    throw new Exception(string.Format(CultureInfo.InvariantCulture, Resources.ErrorWebsiteNotFound, websiteId));
+                }
+
+                var root = GetChildVirtualDirectory(site, "ROOT");
+                if (root == null)
+                {
+                    throw new Exception(Resources.ErrorWebsiteEmpty);
+                }
+
+                var webUiDir = GetChildVirtualDirectory(root, "WebUI");
+                if (webUiDir == null)
+                {
+                    throw new Exception(Resources.ErrorWebUiNotFound);
+                }
+
+                var editorsDir = GetChildVirtualDirectory(webUiDir, "Editors");
+                if (editorsDir == null)
+                {
+                    throw new Exception(Resources.ErrorEditorsVirtualDirNotFound);
+                }
+
+                DirectoryEntry virtualDirectory;
+
+                if (GetChildVirtualDirectory(editorsDir, TargetDirectoryName) == null)
+                {
+                    virtualDirectory = editorsDir.Children.Add(TargetDirectoryName, SchemaNameVirtualDir);
+                    virtualDirectory.Properties["Path"][0] = targetDir + "Editor";
+                    virtualDirectory.CommitChanges();
+
+                    // Remember the path to this virtual directory for later uninstallation / rollback
+                    state.Add(KeyEditorVirtualDirectoryPath, virtualDirectory.Path);
+                }
+
+                var modelsDir = GetChildVirtualDirectory(webUiDir, "Models");
+                if (modelsDir == null)
+                {
+                    throw new Exception(Resources.ErrorModelsVirtualDirNotFound);
+                }
+
+                if (GetChildVirtualDirectory(modelsDir, TargetDirectoryName) == null)
+                {
+                    virtualDirectory = modelsDir.Children.Add(TargetDirectoryName, SchemaNameVirtualDir);
+                    virtualDirectory.Properties["Path"][0] = targetDir + "Model";
+                    virtualDirectory.CommitChanges();
+
+                    // Remember the path to this virtual directory for later uninstallation / rollback
+                    state.Add(KeyModelVirtualDirectoryPath, virtualDirectory.Path);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Deletes the virtual directories created during installation.
+        /// </summary>
+        protected void DeleteVirtualDirectories()
+        {
+            DeleteVirtualDirectory(state[KeyEditorVirtualDirectoryPath] as string);
+            DeleteVirtualDirectory(state[KeyModelVirtualDirectoryPath] as string);
+        }
+
+        /// <summary>
+        /// Deletes a single virtual directory, given its full path.
+        /// </summary>
+        /// <param name="metapath">The full path to the directory (as retrieved by the Path property on DirectoryEntry)</param>
+        protected void DeleteVirtualDirectory(string metapath)
+        {
+            if (string.IsNullOrEmpty(metapath))
+            {
+                return;
+            }
+
+            DirectoryEntry virtualDirectory = new DirectoryEntry(metapath);
+            if (virtualDirectory != null)
+            {
+                using (DirectoryEntry parent = virtualDirectory.Parent)
+                {
+                    parent.Children.Remove(virtualDirectory);
+                    parent.CommitChanges();
+                }
+            }
         }
     }
 }
