9ae03a6cb8fdd89bd90eacd4206e8c8af5d7ef41
pkjaer.sdl@gmail.com <pkjaer.sdl@gmail.com@73b5e0d0-6e95-2095-0101-50d48f5dc44c>
2012-02-07 10:59:20 +0000

    Forgot to check in the new files.

---
+++ PowerTools.Common/Installer/CustomActions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.ComponentModel;
+using System.Security.Permissions;
+
+
+namespace PowerTools.Common.Installer
+{
+    [RunInstaller(true)]
+    public partial class CustomActions : System.Configuration.Install.Installer
+    {
+        public CustomActions()
+        {
+            InitializeComponent();
+        }
+
+        [SecurityPermission(SecurityAction.Demand)]
+        public override void Install(IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+        }
+
+        [SecurityPermission(SecurityAction.Demand)]
+        public override void Commit(IDictionary savedState)
+        {
+            base.Commit(savedState);
+        }
+
+        [SecurityPermission(SecurityAction.Demand)]
+        public override void Rollback(IDictionary savedState)
+        {
+            base.Rollback(savedState);
+        }
+
+        [SecurityPermission(SecurityAction.Demand)]
+        public override void Uninstall(IDictionary savedState)
+        {
+            base.Uninstall(savedState);
+        }
+    }
+}
