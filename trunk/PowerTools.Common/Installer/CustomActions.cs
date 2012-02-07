8fb846276a7c139cbd6bf156b2edb270f45bd111
pkjaer.sdl@gmail.com <pkjaer.sdl@gmail.com@73b5e0d0-6e95-2095-0101-50d48f5dc44c>
2012-02-07 17:25:01 +0000

    Updated the installer to modify System.config too.

--- trunk/PowerTools.Common/Installer/CustomActions.cs
+++ trunk/PowerTools.Common/Installer/CustomActions.cs
@@ -3,8 +3,10 @@ using System.Collections;
 using System.ComponentModel;
 using System.DirectoryServices;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Security.Permissions;
+using System.Xml.Linq;
 
 
 namespace PowerTools.Common.Installer
@@ -17,8 +19,11 @@ namespace PowerTools.Common.Installer
         const string SchemaNameWebDirectory = "IIsWebDirectory";
         const string KeyEditorVirtualDirectoryPath = "PowerTools_EditorVirtualDirectoryPath";
         const string KeyModelVirtualDirectoryPath = "PowerTools_ModelVirtualDirectoryPath";
+        const string EditorAndModelName = "PowerTools";
+        const string VirtualDirectoryName = "PowerTools";
 
         protected IDictionary state;
+        protected XNamespace cfg = "http://www.sdltridion.com/2009/GUI/Configuration";
 
 
         public CustomActions()
@@ -32,6 +37,7 @@ namespace PowerTools.Common.Installer
             state = stateSaver;
             base.Install(stateSaver);
             CreateVirtualDirectories();
+            AddToCmeConfiguration();
         }
 
         [SecurityPermission(SecurityAction.Demand)]
@@ -47,6 +53,7 @@ namespace PowerTools.Common.Installer
             state = savedState;
             base.Rollback(savedState);
             DeleteVirtualDirectories();
+            RemoveFromCmeConfiguration();
         }
 
         [SecurityPermission(SecurityAction.Demand)]
@@ -55,8 +62,39 @@ namespace PowerTools.Common.Installer
             state = savedState;
             base.Uninstall(savedState);
             DeleteVirtualDirectories();
+            RemoveFromCmeConfiguration();
         }
 
+        #region Utility methods
+
+        protected string GetTargetDir()
+        {
+            string result = Context.Parameters["TARGETDIR"] as string;
+            if (string.IsNullOrEmpty(result))
+            {
+                throw new Exception(Resources.ErrorTargetDirNotSet);
+            }
+
+            return result.TrimEnd('\\');
+        }
+
+        protected string GetCmeConfigurationFileName()
+        {
+            string webRoot = Context.Parameters["CME_WEBROOT"] as string;
+            if (string.IsNullOrEmpty(webRoot))
+            {
+                throw new Exception("The WebRoot directory was not passed into the custom action.");
+            }
+
+            webRoot = webRoot.TrimEnd('\\');
+
+            string configurationDir = Path.Combine(webRoot, "Configuration");
+            return Path.Combine(configurationDir, "System.config");
+        }
+
+        #endregion
+
+        #region Virtual directories
 
         /// <summary>
         /// Gets a virtual directory (by name) underneath the given directory.
@@ -92,11 +130,7 @@ namespace PowerTools.Common.Installer
                 throw new Exception(Resources.ErrorWebsiteIdNotSet);
             }
 
-            string targetDir = Context.Parameters["TARGETDIR"] as string;
-            if (string.IsNullOrEmpty(targetDir))
-            {
-                throw new Exception(Resources.ErrorTargetDirNotSet);
-            }
+            string targetDir = GetTargetDir();
 
             using (DirectoryEntry w3svc1 = new DirectoryEntry("IIS://Localhost/W3SVC"))
             {
@@ -129,7 +163,7 @@ namespace PowerTools.Common.Installer
                 if (GetChildVirtualDirectory(editorsDir, TargetDirectoryName) == null)
                 {
                     virtualDirectory = editorsDir.Children.Add(TargetDirectoryName, SchemaNameVirtualDir);
-                    virtualDirectory.Properties["Path"][0] = targetDir + "Editor";
+                    virtualDirectory.Properties["Path"][0] = Path.Combine(targetDir, "Editor");
                     virtualDirectory.CommitChanges();
 
                     // Remember the path to this virtual directory for later uninstallation / rollback
@@ -145,7 +179,7 @@ namespace PowerTools.Common.Installer
                 if (GetChildVirtualDirectory(modelsDir, TargetDirectoryName) == null)
                 {
                     virtualDirectory = modelsDir.Children.Add(TargetDirectoryName, SchemaNameVirtualDir);
-                    virtualDirectory.Properties["Path"][0] = targetDir + "Model";
+                    virtualDirectory.Properties["Path"][0] = Path.Combine(targetDir, "Model");
                     virtualDirectory.CommitChanges();
 
                     // Remember the path to this virtual directory for later uninstallation / rollback
@@ -184,5 +218,69 @@ namespace PowerTools.Common.Installer
                 }
             }
         }
+
+        #endregion
+
+        #region CME configuration (System.config)
+
+        /// <summary>
+        /// Adds the Model and Editor configuration settings to System.config.
+        /// </summary>
+        protected void AddToCmeConfiguration()
+        {
+            string targetDir = GetTargetDir();
+
+            XDocument configuration = XDocument.Load(GetCmeConfigurationFileName());
+
+            var editorsNode = configuration.Descendants(cfg + "editors").FirstOrDefault();
+            var modelsNode = configuration.Descendants(cfg + "models").FirstOrDefault();
+
+            CreateModelOrEditorNode(editorsNode, "editor", EditorAndModelName, Path.Combine(targetDir, "Editor"), @"Configuration\Editor.config", VirtualDirectoryName);
+            CreateModelOrEditorNode(modelsNode, "model", EditorAndModelName, Path.Combine(targetDir, "Model"), @"Configuration\Model.config", VirtualDirectoryName);
+            configuration.Save(GetCmeConfigurationFileName());
+        }
+
+        protected void CreateModelOrEditorNode(XElement parent, string elementName, string name, string installPath, string configFile, string vdir)
+        {
+            var powerToolsNode = new XElement(cfg + elementName);
+            powerToolsNode.SetAttributeValue("name", name);
+
+            var node = new XElement(cfg + "installpath");
+            node.SetValue(installPath);
+            powerToolsNode.Add(node);
+
+            node = new XElement(cfg + "configuration");
+            node.SetValue(configFile);
+            powerToolsNode.Add(node);
+
+            node = new XElement(cfg + "vdir");
+            node.SetValue(vdir);
+            powerToolsNode.Add(node);
+
+            parent.Add(powerToolsNode);
+        }
+
+        /// <summary>
+        /// Removes the Model and Editor configuration settings from System.config.
+        /// </summary>
+        protected void RemoveFromCmeConfiguration()
+        {
+            XDocument configuration = XDocument.Load(GetCmeConfigurationFileName());
+
+            var nodesToRemove = (from node in configuration.Descendants()
+                                 let elemName = node.Name
+                                 let nameAttr = node.Attribute("name")
+                                 where (elemName == cfg + "editor") || (elemName == cfg + "model")
+                                 where nameAttr != null && nameAttr.Value == EditorAndModelName
+                                 select node);
+
+            if (nodesToRemove.Count() > 0)
+            {
+                nodesToRemove.Remove();
+                configuration.Save(GetCmeConfigurationFileName());
+            }
+        }
+
+        #endregion
     }
 }
