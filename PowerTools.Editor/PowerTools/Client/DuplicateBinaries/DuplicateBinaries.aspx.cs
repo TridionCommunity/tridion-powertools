2d3a51f87d6fe57dbe115acf2a8a61eecc8138c3
johnwinter <johnwinter@73b5e0d0-6e95-2095-0101-50d48f5dc44c>
2012-02-23 23:35:44 +0000

    Added the duplicate binaries power tool (still wip!)

---
+++ PowerTools.Editor/PowerTools/Client/DuplicateBinaries/DuplicateBinaries.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using PowerTools.Common.Pages;
+using Tridion.Web.UI.Controls;
+using Tridion.Web.UI.Core.Controls;
+
+namespace PowerTools.Editor.PowerTools.Client.DuplicateBinaries
+{
+	[ControlResourcesDependency(new Type[] { typeof(Popup), typeof(Tridion.Web.UI.Controls.Button), typeof(Stack), typeof(Dropdown), typeof(List) })]
+	[ControlResources("PowerTools.DuplicateBinaries")]
+	public partial class DuplicateBinaries : PowerToolsPageBase
+	{
+	}
+}
