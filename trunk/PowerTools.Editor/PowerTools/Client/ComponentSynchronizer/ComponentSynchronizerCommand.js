b93d31c18022d0ecca0caa2f0d568638caca244f
pkjaer.sdl@gmail.com <pkjaer.sdl@gmail.com@73b5e0d0-6e95-2095-0101-50d48f5dc44c>
2012-04-06 12:57:19 +0000

    Merged in the Component Synchronizer changes from Jaime's branch.

--- trunk/PowerTools.Editor/PowerTools/Client/ComponentSynchronizer/ComponentSynchronizerCommand.js
+++ trunk/PowerTools.Editor/PowerTools/Client/ComponentSynchronizer/ComponentSynchronizerCommand.js
@@ -1,66 +1,164 @@
 ﻿Type.registerNamespace("PowerTools.Commands");
 
-PowerTools.Commands.ComponentSynchronizer = function ()
-{
+/**
+* Implements Component Synchronizer command.
+* @constructor
+*/
+PowerTools.Commands.ComponentSynchronizer = function () {
     Type.enableInterface(this, "PowerTools.Commands.ComponentSynchronizer");
     this.addInterface("Tridion.Cme.Command", ["ComponentSynchronizer"]);
     this.addInterface("PowerTools.ToolBase", ["ComponentSynchronizer"]);
+    this._isPopupOpen = false;
+    this._currentSchema = null;
+    this._currentPublication = null;
 };
 
+
+/**
+* Checks wether a command is available or not.
+* @param {Tridion.Cme.Selection] selection. The selected items in the list
+*/
 PowerTools.Commands.ComponentSynchronizer.prototype.isAvailable = function (selection)
-{
-    //    //Only show the button if a single FOLDER is selected
-    //    if (selection.getCount() == 1) {
-    //        var itemType = $models.getItemType(selection.getItem(0));
-    //        var item = $models.getItem(selection.getItem(0))
-    //        if (itemType == $const.ItemType.FOLDER) {
-    //            return true;
-    //        }
-    //    }
+{   
     return this._defineEnabled(selection);
 };
 
+/**
+* Checks wether a command is enabled or not.
+* @param {Tridion.Cme.Selection] selection. The selected items in the list
+*/
 PowerTools.Commands.ComponentSynchronizer.prototype.isEnabled = function (selection)
 {
     return this._defineEnabled(selection);
 };
 
-PowerTools.Commands.ComponentSynchronizer.prototype._execute = function (selection)
-{
-    var uriSelection = selection.getItem(0);
-    var baseElement = $("#contentsplitter_container");
-    var iFrame = $("#CustomPagesFrame");
-    var self = this;
+/**
+* Executes the command.
+* @param {Tridion.Cme.Selection] selection. The selected items in the list
+*/
+PowerTools.Commands.ComponentSynchronizer.prototype._execute = function (selection) {
 
-    //We are passing the uri selection. Needed? Maybe Component Synchronizer can be activated for schemas.
-    var PopUpUrl = $ptUtils.expandPath("/PowerTools/Client/ComponentSynchronizer/ComponentSynchronizer.aspx") + "#folderId=" + uriSelection;
-    this._popup = $popup.create(PopUpUrl, "toolbar=no,width=600px,height=400px,resizable=1,scrollbars=1", null);
-
-    $evt.addEventHandler(this._popup, "close", this.getDelegate(this._onPopupClose));
-
-    this._popup.open();
-};
-
-
-PowerTools.Commands.ComponentSynchronizer.prototype._onPopupClose = function ()
-{
-    $evt.removeAllEventHandlers(this._popup);
-    this._popup.dispose();
-    this._popup = null;
-
-};
-
-
-PowerTools.Commands.ComponentSynchronizer.prototype._defineEnabled = function (selection)
-{
-    var treeView = $controls.getControl($("#DashboardTree"), "Tridion.Controls.FilteredTree");
-    //var selection = treeView.getSelection().getItem(0);
-    var item = selection.getItem(0);
-    var itemType = $models.getItemType(item);
-
-    if (itemType == $const.ItemType.SCHEMA)
-    {
-        return true;
+    if (this._isPopupOpen) {
+        return;
     }
-    return false;
+    var validSelection = this.isValidSelection(selection);
+
+    if (validSelection) {
+        var uriSelection = selection.getItem(0);
+        var baseElement = $("#contentsplitter_container");
+        var iFrame = $("#CustomPagesFrame");
+        var self = this;
+
+        var PopUpUrl = $ptUtils.expandPath("/PowerTools/Client/ComponentSynchronizer/ComponentSynchronizer.aspx"); // +"#folderId=" + uriSelection;
+        //TODO: USING ROOT FOLDER FOR TEMPORARY LOCATION
+        
+
+        var publication = $models.getItem(this._currentPublication);
+        this._rootFolder = publication.getRootFolderId();
+
+        $log.message("Opening Popup for Schema: " + this._currentSchema);
+        this._popup = $popup.create(PopUpUrl, "toolbar=no,width=750px,height=630px,resizable=false,scrollbars=false", { sel: selection, schema: this._currentSchema, pub: this._currentPublication, folder: this._rootFolder});
+        $evt.addEventHandler(this._popup, "unload", this.getDelegate(this._onPopupCanceled));
+
+
+        this._popup.open();
+        this._isPopupOpen = true;
+
+    }
+    else {
+        var msg = $messages.createMessage("Tridion.Cme.Model.WarningMessage", "SELECTION MUST BE EITHER: ", "- A Schema \r\n - A Component \r\n - Multiple Components based on the same Schema \r\n ", true, true);
+        $messages.registerMessage(msg);
+    }
+
+};
+
+
+/**
+* On Popup Canceled Event. 
+* @param {Tridion.Core.Event}. The cancel/unload event.
+*/
+PowerTools.Commands.ComponentSynchronizer.prototype._onPopupCanceled = function (event) {    
+    this._popup.dispose();    
+    this._popup = null;    
+    this._isPopupOpen = false;
+    
+}
+
+
+/**
+* On Refresh Button Click. 
+* @param {Tridion.Core.Event}. The click event.
+*/
+PowerTools.Commands.ComponentSynchronizer.prototype.isValidSelection = function (sel) {
+    var items = sel.getItems();
+    if (items.length > 1) {
+        var firstSchema = '';
+        var currentPub = '';
+        for (var i = 0, len = items.length; i < len; i++) {
+
+            var itemId = sel.getItem(i);
+            var item = $models.getItem(itemId);
+
+            if (item) {
+                if (item.getItemType() != $const.ItemType.COMPONENT) {
+                    return false;
+                }
+                if (i == 0) {
+                    firstSchema = item.getSchema().getId();
+                    currentPub = item.getPublicationId();
+                }
+                if (i > 0 && item.getSchema().getId() != firstSchema) {
+                    return false;
+                }
+            }
+
+        }
+        this._currentSchema = firstSchema;
+        this._currentPubilcation = currentPub;
+    }
+    return true;
+}
+
+
+/**
+* Checks wether the Command is Enabled/Available or not
+* @param {Tridion.Cme.Selection] selection. The selected items in the list
+*/
+PowerTools.Commands.ComponentSynchronizer.prototype._defineEnabled = function (selection) {
+    if (!selection) {
+        return false;
+    }
+    var items = selection.getItems();
+    if (items.length == 0) {
+        //Nothing Selected
+        return false;
+    } else if (items.length == 1) {
+        var itemId = selection.getItem(0);
+        var item = $models.getItem(itemId);
+        if (item) {
+            if (item.getItemType() != $const.ItemType.SCHEMA && item.getItemType() != $const.ItemType.COMPONENT) {
+                return false;
+            } else {
+                this._currentPublication = item.getPublicationId();
+                if (item.getItemType() == $const.ItemType.SCHEMA) {
+                    this._currentSchema = item.getId();                    
+                } else {
+                    this._currentSchema = item.getSchema().getId();
+                }
+            }
+        }
+    } else {
+        for (var i = 0, len = items.length; i < len; i++) {
+            var itemId = selection.getItem(0);
+            var item = $models.getItem(itemId);
+
+            if (item) {
+                if (item.getItemType() != $const.ItemType.COMPONENT) {
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+
 }
