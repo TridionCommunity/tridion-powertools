ca4beb9fe159e415e0908446d196d23b97e9f9d8
s990330@gmail.com <s990330@gmail.com@73b5e0d0-6e95-2095-0101-50d48f5dc44c>
2012-06-24 19:39:51 +0000

    --> Fix for issue #33 (Uncaught exception when a newly created mm-component has a title that already exists in the folder)

---
+++ trunk/PowerTools.Model/Utils/TridionNamespaceManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml.Linq;
+
+namespace PowerTools.Model.Utils
+{
+    public static class TridionNamespaceManager
+    {
+
+        public static XNamespace Tcm = "http://www.tridion.com/ContentManager/5.0";
+
+    }
+}
