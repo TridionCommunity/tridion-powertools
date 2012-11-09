28d248585e0c27ac502ad1eb9a701fd279baba94
arthurtheaquaboy <arthurtheaquaboy@73b5e0d0-6e95-2095-0101-50d48f5dc44c>
2012-11-09 19:47:24 +0000

    Updated for Workflow Notification UI

---
+++ PowerTools.Editor/PowerTools/Client/WorkflowNotificationSettings/WorkflowNotificationSettingsPopup.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Title="User Workflow Notification Settings" Language="C#" MasterPageFile="../Shared/Views/PopUp.Master"
+	AutoEventWireup="true" CodeBehind="WorkflowNotificationSettingsPopup.aspx.cs" Inherits="PowerTools.Editor.PowerTools.Client.WorkflowNotificationSettings.WorkflowNotificationSettingsPopup" %>
+
+<asp:Content ID="WorkflowNotificationSettings" ContentPlaceHolderID="Main" runat="server">
+	<div class="tool-explanation">
+		Allows a user to set thier notification settings
+	</div>
+	
+	<hr class="br" />
+	<div>
+		<textarea rows="10" id="UserSettings" style="width:99%">Loading current settings...</textarea>
+	</div>
+    <c:button id="SaveButton" runat="server" label="Save Changes" class="customButton"/>
+</asp:Content>
---
+++ PowerTools.Editor/PowerTools/Client/WorkflowNotificationSettings/WorkflowNotificationSettingsPopup.js
@@ -0,0 +1,63 @@
+﻿Type.registerNamespace("PowerTools.Popups");
+
+// Construct new instance of this popup
+PowerTools.Popups.WorkflowNotificationSettings = function (element) {
+    Type.enableInterface(this, "PowerTools.Popups.WorkflowNotificationSettings");
+    this.addInterface("Tridion.Cme.View");
+};
+
+// Attach event handling for Refresh button and initiate execution
+PowerTools.Popups.WorkflowNotificationSettings.prototype.initialize = function () {
+    this.callBase("Tridion.Cme.View", "initialize");
+    var p = this.properties;
+    var c = p.controls;
+    c.SaveButton = $controls.getControl($("#SaveButton"), "Tridion.Controls.Button");
+    $evt.addEventHandler(c.SaveButton, "click", this.getDelegate(this._saveAppData));
+    var onSuccess = Function.getDelegate(this, this.showCurrentSettings);
+    var uriCurrentUser = Tridion.UI.UserSettings.getJsonUserSettings(true).User["@ID"];
+    $messages.registerNotification("loading user workflow notification settings");
+    PowerTools.Model.Services.AppDataServices.Read("code.google.com/p/tridion-notification-framework", uriCurrentUser, onSuccess, null, null, false);
+    c.SaveButton.enable();
+};
+
+
+PowerTools.Popups.WorkflowNotificationSettings.prototype._saveAppData = function () {
+    var onSuccess = Function.getDelegate(this, this._handleSave);
+    var uriCurrentUser = Tridion.UI.UserSettings.getJsonUserSettings(true).User["@ID"];
+    var data = $('#UserSettings').value;
+    PowerTools.Model.Services.AppDataServices.Save("code.google.com/p/tridion-notification-framework", uriCurrentUser, data, onSuccess, null, null, false);
+};
+
+PowerTools.Popups.WorkflowNotificationSettings.prototype._handleSave = function (result) {
+    if (result == "true") {
+        $messages.registerGoal("workflow notification settings saved...")
+        window.close();
+    } else {
+        $messages.registerError("workflow notification settings could not be saved")
+    }
+}
+
+
+PowerTools.Popups.WorkflowNotificationSettings.prototype.showCurrentSettings = function (result) {
+    var userSettingsXML = $xml.getNewXmlDocument(result);
+    var userSettingsTextarea = $('#UserSettings');
+    var appDataString = '';
+    try {
+        appDataString = new XMLSerializer().serializeToString(userSettingsXML.firstChild.firstChild);
+    } catch (e) {
+        //Then we load the default XML
+        var xmlHTTP = new XMLHttpRequest();
+        xmlHTTP.open("GET", "SampleAppData.xml", false);
+        xmlHTTP.send(null);
+        appDataString = xmlHTTP.responseText;
+    }
+    try {
+        userSettingsTextarea.value = XML(appDataString).toXMLString();
+    } catch (e) {
+        userSettingsTextarea.value = appDataString;
+    }
+
+};
+
+// Register this popup with the framework
+$display.registerView(PowerTools.Popups.WorkflowNotificationSettings);
