3b142b1173b28441a9f347525745c2415686ce13
arthurtheaquaboy <arthurtheaquaboy@73b5e0d0-6e95-2095-0101-50d48f5dc44c>
2013-02-12 16:12:08 +0000

    Updated JS to remove the hard coded Editor Path

--- PowerTools.Editor/PowerTools/Client/DocumentItem/DocumentItemCommand.js
+++ PowerTools.Editor/PowerTools/Client/DocumentItem/DocumentItemCommand.js
@@ -17,8 +17,8 @@ PowerTools.Commands.DocumentItem.prototype.isValidSelection = function (selectio
 	return ($models.getItemType(itemID) == $const.ItemType.SCHEMA);
 };
 
-PowerTools.Commands.DocumentItem.prototype._execute = function ExportToWord$_execute(selection) 
-{
+PowerTools.Commands.DocumentItem.prototype._execute = function ExportToWord$_execute(selection) {
     var itemID = selection.getItem(0);
-    window.location = document.location.protocol + "//" + document.location.host + "/WebUI/Editors/PowerTools/PowerTools/Client/DocumentItem/SchemaDefinition.docx?id=" + itemID;
+    var path = $config.expandEditorPath("/PowerTools/Client/DocumentItem/SchemaDefinition.docx?id=" + itemID, "PowerTools");
+    window.location = document.location.protocol + "//" + document.location.host + path;
 };
