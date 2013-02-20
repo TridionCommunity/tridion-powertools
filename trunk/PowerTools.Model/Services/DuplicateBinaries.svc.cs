f66d932e0838d49633e35dfc3d01c7d6687a13fc
johnwinter@gmail.com <johnwinter@gmail.com@73b5e0d0-6e95-2095-0101-50d48f5dc44c>
2013-02-20 01:02:27 +0000

    Returns a DuplicateBinariesData object - detailing the list of duplicate items in the publication

--- trunk/PowerTools.Model/Services/DuplicateBinaries.svc.cs
+++ trunk/PowerTools.Model/Services/DuplicateBinaries.svc.cs
@@ -1,14 +1,14 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.ServiceModel;
 using System.ServiceModel.Activation;
 using System.ServiceModel.Web;
 using System.Xml;
-using System.Xml.Linq;
-using System.Xml.XPath;
 using PowerTools.Common.CoreService;
 using PowerTools.Model.Progress;
 using Tridion.ContentManager.CoreService.Client;
+using System.Xml.Linq;
 
 namespace PowerTools.Model.Services
 {
@@ -21,12 +21,18 @@ namespace PowerTools.Model.Services
     [ServiceContract(Namespace = "PowerTools.Model.Services")]
     public class DuplicateBinaries : BaseService
     {
-        private IDictionary<string, string> fileNames = new Dictionary<string, string>();
-        private IDictionary<string, string> badFileNames = new Dictionary<string, string>();
+        private Dictionary<string, string> fileNames;
+        private List<DuplicateBinariesData> _duplicateData = null; // list returned containing duplicate data
+
+        /// <summary>
+        /// DuplicateBinariesParameters
+        /// PublicationId - the ID of the publication used to search through the XML
+        /// </summary>
         class DuplicateBinariesParameters
         {
             public string PublicationId { get; set; }
         }
+
 
         /// <summary>
         /// Service operation that searches all the binary components to check if some items contain the same file names.
@@ -35,7 +41,7 @@ namespace PowerTools.Model.Services
         /// <param name="includeBlueprinting">Boolean value </param>
         /// <returns></returns>
         [OperationContract, WebGet(ResponseFormat = WebMessageFormat.Json)]
-        public ServiceProcess Execute(string publicationId, bool includeBlueprinting)
+        public ServiceProcess Execute(string publicationId)
         {
 
             if (string.IsNullOrEmpty(publicationId))
@@ -56,11 +62,16 @@ namespace PowerTools.Model.Services
             return base.GetProcessStatus(Id);
         }
 
-
         public override void Process(ServiceProcess process, object arguments)
         {
+            _duplicateData = new List<DuplicateBinariesData>();
+            fileNames = new Dictionary<string, string>();
             DuplicateBinariesParameters parameters = (DuplicateBinariesParameters)arguments;
-			process.SetCompletePercentage(10);
+            
+            //_duplicateBinariesData = new DuplicateBinariesData();
+            //_duplicateBinariesData.PublicationId = parameters.PublicationId;
+            
+            process.SetCompletePercentage(10);
             process.SetStatus("working");
 
             using (var coreService = Client.GetCoreService())
@@ -71,53 +82,78 @@ namespace PowerTools.Model.Services
                     process.SetStatus("Creating publication filter");
 
                     // Create a filter to only fetch multimedia components from the publication
-                    ItemsFilterData filter = new ItemsFilterData();
+                    RepositoryItemsFilterData filter = new RepositoryItemsFilterData();
                     filter.ItemTypes = new[] { ItemType.Component };
                     filter.ComponentTypes = new[] { ComponentType.Multimedia };
-
                     XElement mmComponentsListXml = coreService.GetListXml(parameters.PublicationId, filter);
+			        XNamespace tcm = "http://www.tridion.com/ContentManager/5.0";
 
-                    XmlNamespaceManager nsMgr = new XmlNamespaceManager(new NameTable());
-                    nsMgr.AddNamespace("tcm", "http://www.tridion.com/ContentManager/5.0");
+                    double progressIncrement = mmComponentsListXml.Value.Length == 0 ? 0 : 80 / mmComponentsListXml.Value.Length; //nasty progress calculation
+                    int i = 1;
 
-                    foreach (XElement itemElem in mmComponentsListXml.XPathSelectElements("/tcm:item", nsMgr))
+                    // keep a list of all the file names from the items in the publication
+                    // the if a file already exists in the filenames, it is considered a 'duplicate file name'
+                    foreach(XElement itemElem in mmComponentsListXml.Descendants(tcm + "Item"))
                     {
                         string itemId = itemElem.Attribute("ID").Value;
                         string binaryFileName = GetFileNameFromComponent(coreService, itemId);
-                        if (!fileNames.ContainsKey(binaryFileName))
+                        
+                        fileNames.Add(itemId, binaryFileName);
+
+                        int progressPercentage = (int)(20 + i * progressIncrement); // some more nasty progress calculation
+                        process.SetCompletePercentage(progressPercentage);
+                        i++;
+                    }
+
+                    var duplicateValues = fileNames.ToLookup(a => a.Value).
+                        Where(b => b.Count() > 1);
+
+                    // todo - refactor this below item to select the id's and values from the file
+                    // name list
+
+                    foreach (var group in duplicateValues)
+                    {
+                        foreach (KeyValuePair<string, string> kvp in group)
                         {
-                            fileNames.Add(binaryFileName, itemId);
-                            //Console.WriteLine(GetFileNameFromComponent(client, itemElem.Attribute("ID").Value));
-                        }
-                        else
-                        {
-                            badFileNames.Add(binaryFileName, itemId);
-                            Console.WriteLine(binaryFileName);
+                            _duplicateData.Add(new DuplicateBinariesData
+                            {
+                                ItemTcmId = kvp.Key,
+                                ItemFileName = kvp.Value,
+                            });
                         }
                     }
 
-                    process.SetCompletePercentage(75);
-                    process.SetStatus("Extracting item counts");
-
-                    process.Complete("hi"); //mmComponentsListXml.OuterXml);
+                    process.Complete("Done");
                 }
                 catch (Exception ex)
                 {
-                    process.Complete(ex.Message);
+                    // TODO: Update the GUI that there has been error - solution below is temporary
+                    process.Failed = true;
+                    process.Complete(string.Format("Failure finding duplicate items reason: {0}", ex.Message));
+                    return;
+                    
                 }
-            
+
             }
 
 
         }
 
+        /// <summary>
+        /// Return the response object with the actual counts in it.
+        /// This method returns null if the process has not completed yet.
+        /// </summary>
+        /// <returns>DuplicateBinaryData object with the actual list of filenames, or null if process is still running</returns>
+        [OperationContract, WebGet(ResponseFormat = WebMessageFormat.Json)]
+        public List<DuplicateBinariesData> GetDuplicateBinaryData()
+        {
+            return _duplicateData;
+        }
+
         private string GetFileNameFromComponent(SessionAwareCoreServiceClient client, string componentId)
         {
             ComponentData compData = (ComponentData)client.Read(componentId, new ReadOptions());
-            return "comp data = " + compData.BinaryContent.Filename;
+            return compData.BinaryContent.Filename;
         }
     }
 }
-
-
-
