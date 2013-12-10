0fbf2d605bfd7ac9b00c20c4132d0a6ccac9c891
pkjaer.sdl@gmail.com <pkjaer.sdl@gmail.com@73b5e0d0-6e95-2095-0101-50d48f5dc44c>
2013-12-10 07:49:59 +0000

    Formatting according to our guidelines.

--- PowerTools.Editor/PowerTools/Client/AddUser/AddUserCommand.js
+++ PowerTools.Editor/PowerTools/Client/AddUser/AddUserCommand.js
@@ -1,15 +1,18 @@
 ﻿Type.registerNamespace("PowerTools.Commands");
 
-PowerTools.Commands.AddUser = function () {
+PowerTools.Commands.AddUser = function() 
+{
     Type.enableInterface(this, "PowerTools.Commands.AddUser");
     this.addInterface("PowerTools.BaseCommand", ["AddUser"]);
 };
 
-PowerTools.Commands.AddUser.prototype.isValidSelection = function (selection) {
+PowerTools.Commands.AddUser.prototype.isValidSelection = function(selection) 
+{
     return Tridion.Cme.Commands.New.prototype._canCreateUser(selection);
 }
 
-PowerTools.Commands.AddUser.prototype._execute = function (selection) {
+PowerTools.Commands.AddUser.prototype._execute = function(selection) 
+{
     var userName = prompt("Please enter a username e.g. 'DOMAIN\\USER'", "");
     var onFailure = Function.getDelegate(this, this._handleFailure);
     var onSuccess = Function.getDelegate(this, this._handleSuccess);
@@ -18,17 +21,22 @@ PowerTools.Commands.AddUser.prototype._execute = function (selection) {
     PowerTools.Model.Services.AddUser.CreateUser(userName, onSuccess, onFailure, context, false);
 };
 
-PowerTools.Commands.AddUser.prototype._handleSuccess = function (result) {
-    if (result.ErrorMessage == undefined) {
+PowerTools.Commands.AddUser.prototype._handleSuccess = function(result) 
+{
+    if (result.ErrorMessage == undefined) 
+	{
         $messages.registerGoal("AddUser Powertool: created " + result.UserName);
-        $display.getView().refreshList()
-        //$cme.getCommand("Refresh")._execute();
-        
-    } else {
+	    $display.getView().refreshList();
+	    //$cme.getCommand("Refresh")._execute();
+
+	} 
+	else 
+	{
         $messages.registerError("AddUser Powertool: "+ result.ErrorMessage);
     }
 };
 
-PowerTools.Commands.AddUser.prototype._handleFailure = function (result) {
+PowerTools.Commands.AddUser.prototype._handleFailure = function (result) 
+{
     $messages.registerError("AddUser Powertool: " + result.ErrorMessage);
 };
