9cdddd5cdc747496cd3daf85f2a8c860802c74c5
pkjaer.sdl@gmail.com <pkjaer.sdl@gmail.com@73b5e0d0-6e95-2095-0101-50d48f5dc44c>
2013-12-10 06:59:27 +0000

    Fixed the App Data Inspector popup.

--- trunk/PowerTools.Editor/PowerTools/Client/AppDataInspector/AppDataInspectorPopup.aspx.cs
+++ trunk/PowerTools.Editor/PowerTools/Client/AppDataInspector/AppDataInspectorPopup.aspx.cs
@@ -6,7 +6,7 @@ using Tridion.Web.UI.Core.Controls;
 namespace PowerTools.Editor.PowerTools.Client.AppDataInspector
 {
 
-	[ControlResourcesDependency(new Type[] { typeof(Popup), typeof(Tridion.Web.UI.Controls.Button), typeof(Stack), typeof(Dropdown), typeof(List) })]
+	[ControlResourcesDependency(new[] { typeof(Popup), typeof(Button), typeof(Stack), typeof(Dropdown), typeof(List) })]
 	[ControlResources("PowerTools.AppDataInspectorPopup")]
 	public partial class AppDataInspectorPopup : PowerToolsPageBase
 	{
