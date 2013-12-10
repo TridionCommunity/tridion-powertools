fcf3b6cdc2e0a0f5d1e504dd220eee00b4ad4390
pkjaer.sdl@gmail.com <pkjaer.sdl@gmail.com@73b5e0d0-6e95-2095-0101-50d48f5dc44c>
2013-12-10 07:12:51 +0000

    Fixed JavaScript errors when loading the CME. Also formatted the document according to our coding guidelines (it's still a very non-standard implementation though - needs to be fixed at some point)

--- trunk/PowerTools.Editor/PowerTools/Client/DragDropUploader/DragDropUploader.js
+++ trunk/PowerTools.Editor/PowerTools/Client/DragDropUploader/DragDropUploader.js
@@ -1,35 +1,40 @@
 ﻿///DragDropUploader version 0.1
-
 $evt.addEventHandler($display, "start", DragDropUploader$onDisplayStarted);
 
-function DragDropUploader$onDisplayStarted() {
+function DragDropUploader$onDisplayStarted() 
+{
     $evt.removeEventHandler($display, "start", DragDropUploader$onDisplayStarted);
 
-    //IE does not support drag and drop and multiple file selection (yet)     
-    if ($dom.isIE) {
+    // IE does not support drag and drop and multiple file selection (yet)
+    if ($dom.isIE) 
+	{
         return;
     }
    
     var toolbar = $controls.getControl($("#DashboardToolbar"), "Tridion.Controls.RibbonToolbar");
-    var page = toolbar.getPage("DashboardPage");
-    
-    //Initialize the DragAndDropUploader after the frame has loaded
-    $evt.addEventHandler(page, "frameloaded", _onTridionDashboardFrameLoaded);
+	if (toolbar)
+	{
+		var page = toolbar.getPage("DashboardPage");
+		if (page)
+		{
+			//Initialize the DragAndDropUploader after the frame has loaded
+			$evt.addEventHandler(page, "frameloaded", _onTridionDashboardFrameLoaded);
+		}
+	}
 }
 
-function _onTridionDashboardFrameLoaded() {
+function _onTridionDashboardFrameLoaded() 
+{
     var ddUploader = new PowerTools.AddOns.DragDropUploader.Base();
     ddUploader.initialize();
 }
 
 Type.registerNamespace("PowerTools.AddOns.DragDropUploader");
-PowerTools.AddOns.DragDropUploader.Base = function () {
-
-    console.log("Initializing DragDropUploader...");
+PowerTools.AddOns.DragDropUploader.Base = function () 
+{
     Type.enableInterface(this, "PowerTools.AddOns.DragDropUploader");
 
     //var filteredList = $controls.getControl($("#FilteredDashboardList"), "Tridion.Controls.FilteredList");
-
 
     this._strings =
     {
@@ -47,16 +52,15 @@ PowerTools.AddOns.DragDropUploader.Base = function () {
         File: "file"
     };
 
-    this._controls =
-    {
-        SchemaControl: null
-    };
+    this._controls = { SchemaControl: null };
 
-    this.initialize = function () {
+    this.initialize = function () 
+	{
         var context = this;
 
         //Load style
-        $j.get(context._strings.UrlCss, function (css) {
+        $j.get(context._strings.UrlCss, function (css) 
+		{
             $j('<style type="text/css"></style>')
               .html(css)
               .appendTo("head");
@@ -70,13 +74,15 @@ PowerTools.AddOns.DragDropUploader.Base = function () {
         context._initMinMaxButton();
 
         //Bind 'refresh' event
-        var tree = $controls.getControl($("#DashboardTree"), "Tridion.Controls.FilteredTree")
-        $evt.addEventHandler(tree, "select", function () {
+	    var tree = $controls.getControl($("#DashboardTree"), "Tridion.Controls.FilteredTree");
+        $evt.addEventHandler(tree, "select", function() 
+		{
             var tree = $controls.getControl($("#DashboardTree"), "Tridion.Controls.FilteredTree");
             var selection = tree.getSelection().getItem(0);
 
             //If selected item is in a different publication than the currenty loaded schema, refresh it
-            if ($ptUtils.getPublicationIdFromTcmUri(selection) != $ptUtils.getPublicationIdFromTcmUri(context._controls.SchemaControl.getValue())) {
+            if ($ptUtils.getPublicationIdFromTcmUri(selection) != $ptUtils.getPublicationIdFromTcmUri(context._controls.SchemaControl.getValue())) 
+			{
                 context._controls.SchemaControl.unload(false);
                 context._controls.SchemaControl.reset();
             }
@@ -85,34 +91,40 @@ PowerTools.AddOns.DragDropUploader.Base = function () {
 
     };
 
-    this._appendUploadTemplate = function () {
+    this._appendUploadTemplate = function() 
+	{
         //Load and append the upload-html to the FilteredDashboardList       
         var urlToUploadHtml = this._strings.UrlUploadTemplate;
         var template = PowerTools.Utilities.prototype.getTemplate(urlToUploadHtml);
         $j("#FilteredDashboardList").append(template);
     };
 
-    this._initSchemaDropDown = function () {
+    this._initSchemaDropDown = function() 
+	{
         var context = this;
         this._controls.SchemaControl = $controls.getControl($("#SchemaDropDownForDdu"), "Tridion.Controls.Dropdown");
 
         //Load Multimedia schema dropdown
-        $evt.addEventHandler(this._controls.SchemaControl, "loadcontent",
-        function () {
+        $evt.addEventHandler(this._controls.SchemaControl, "loadcontent", function() 
+		{
             var folder = $models.getItem($url.getHashParam("locationId"));
             var publication = folder.getPublication();
             var schemaList = publication.getListSchemas($const.SchemaPurpose.MULTIMEDIA);
 
-            if (schemaList) {
-                var schemaListLoaded = function () {
+            if (schemaList) 
+			{
+                var schemaListLoaded = function () 
+				{
                     $evt.removeEventHandler(schemaList, "load", schemaListLoaded);
                     context._controls.SchemaControl.setContent(schemaList.getXml());
                 };
 
-                if (schemaList.isLoaded(true)) {
+                if (schemaList.isLoaded(true)) 
+				{
                     schemaListLoaded();
                 }
-                else {
+                else 
+				{
                     $evt.addEventHandler(schemaList, "load", schemaListLoaded);
                     schemaList.load();
                 }
@@ -121,36 +133,47 @@ PowerTools.AddOns.DragDropUploader.Base = function () {
 
     };
 
-    this._initUploadArea = function () {
-
+    this._initUploadArea = function() 
+	{
         var context = this;
         var postUrl = this._strings.UrlUploadHandler;
-        $j(function () {
+        $j(function() 
+		{
             var msg = null;
-            $j('#fileupload').fileupload({
+            $j('#fileupload').fileupload(
+			{
                 dataType: 'json',
                 url: postUrl,
                 singleFileUploads: false,
-                done: function (e, data) {
+                done: function (e, data) 
+				{
                     msg.finish();
                     var success = [];
                     var fail = [];
-                    $j.each(data.result, function (index, file) {
-                        //console.log(file);
-                        if (file.uploadSuccess) {
+                    $j.each(data.result, function (index, file) 
+					{
+                        if (file.uploadSuccess) 
+						{
                             success.push(file);
-                        } else {
+                        } 
+						else 
+						{
                             fail.push(file);
                         }
                     });
 
-                    if (fail.length == 0) {
+                    if (fail.length == 0) 
+					{
                         $messages.registerGoal(context._strings.MessageSuccess.format(data.files.length, (data.files.length > 1 ? "files" : "file")), null, false, false);
-                    } else {
-                        var failedMessage = context._strings.MessageFailureDetails + ": \n\r"
-                        for (var i = 0, k = fail.length; i < k; i++) {
-                            failedMessage += "-> {0}".format(fail[i].name)
-                            if (i < (k - 1)) {
+                    } 
+					else
+                    {
+	                    var failedMessage = context._strings.MessageFailureDetails + ": \n\r";
+                        for (var i = 0, k = fail.length; i < k; i++)
+                        {
+	                        failedMessage += "-> {0}".format(fail[i].name);
+                            if (i < (k - 1)) 
+							{
                                 failedMessage += "\n\r ";
                             }
                         }
@@ -158,65 +181,80 @@ PowerTools.AddOns.DragDropUploader.Base = function () {
 
                     }
                     //Only refresh if the current view is the orgitem-view the files were uploade to
-                    if (data.formData.orgItemId == $url.getHashParam("locationId")) {
+                    if (data.formData.orgItemId == $url.getHashParam("locationId")) 
+					{
                         $display.getView().refreshList();
                     }
                 },
-                dragover: function (e) {
-                    var dropZone = $j('#fileupload'),
-                        timeout = window.dropZoneTimeout;
-                    if (!timeout) {
+                dragover: function (e)
+                {
+	                var dropZone = $j('#fileupload');
+					var timeout = window.dropZoneTimeout;
+
+                    if (!timeout) 
+					{
                         dropZone.addClass('in');
-                    } else {
+                    } 
+					else 
+					{
                         clearTimeout(timeout);
                     }
-                    if (e.target === dropZone[0]) {
+                    if (e.target === dropZone[0]) 
+					{
                         dropZone.addClass('hover');
-                    } else {
+                    } 
+					else 
+					{
                         dropZone.removeClass('hover');
                     }
-                    window.dropZoneTimeout = setTimeout(function () {
+                    window.dropZoneTimeout = setTimeout(function() 
+					{
                         window.dropZoneTimeout = null;
                         dropZone.removeClass('in hover');
                     }, 100);
                 },
-                fail: function (e, data) {
+                fail: function (e, data) 
+				{
                     msg.finish();
                     $messages.registerError(this._strings.MessageError);
                 },
-                drop: function (e, data) {
+                drop: function (e, data) 
+				{
                     //if no schema is selected, don't upload anything. 
                     //Schema check may not be needed in the future. (eg: uploading pages, keywords, etc)
-                    if (!context._controls.SchemaControl.getValue()) {
+                    if (!context._controls.SchemaControl.getValue()) 
+					{
                         $messages.registerError(context._strings.MessageSelectSchema);
                         return false;
                     }
                 },
-                submit: function (e, data) {
+                submit: function (e, data) 
+				{
                     //Append shemaUri and orgitemId to the post-data
                     data.formData = { orgItemId: $url.getHashParam("locationId"), schemaId: context._controls.SchemaControl.getValue() };
                     msg = $messages.registerProgress(context._strings.MessageProgress.format(data.files.length, (data.files.length > 1 ? context._strings.Files : context._strings.File)), context._strings.MessageTitle, false, false, false);
                 }
             });
         });
-
-
     };
 
     //Add eventhandler to minmax button click
-    this._initMinMaxButton = function () {
-        $j("#minmax").live("click", function () {
-            $j("#fileupload").slideToggle("slow", function () {
-                if ($j(this).is(":visible")) {
+    this._initMinMaxButton = function () 
+	{
+        $j("#minmax").live("click", function () 
+		{
+            $j("#fileupload").slideToggle("slow", function () 
+			{
+                if ($j(this).is(":visible")) 
+				{
                     $j("#minmax").css("bottom", "103px");
-                } else {
+                } 
+				else 
+				{
                     $j("#minmax").css("bottom", "1px");
                 }
             });
         });
     };
-
-
-
 };
 
